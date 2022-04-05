6bea91432d050d4b58d0a6a2bdf10b06c55f696f
kerimovscreations <kerimovscreations@gmail.com>
2022-04-05 20:38:50 +0400

    initial commit

--- Session 1/ViewController.swift
+++ Session 1/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("viewDidLoad")
+        
+        self.view.backgroundColor = .white
+        
+        print("Width: \(self.view.frame.width)")
+        print("Height: \(self.view.frame.height)")
+        
+        let label = UILabel()
+        label.frame = CGRect.init(x: 100, y: 100, width: 100, height: 30)
+        label.text = "Test1 Test1 Test1 Test1"
+        label.textColor = .red
+        label.backgroundColor = .lightGray
+        self.view.addSubview(label)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewWillAppear")
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print(#function)
+    }
 
 }
 
