781f9cba39352530b3f16576338e6cdb8e0f70b8
kerimovscreations <kerimovscreations@gmail.com>
2022-04-16 20:36:41 +0400

    session 4

--- Session 1/ViewController.swift
+++ Session 1/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
@@ -13,161 +14,56 @@ class ViewController: UIViewController {
         
     // MARK: - UI Components
     
-    private lazy var view1: UIView = {
-        let view = UIView()
-        view.backgroundColor = .gray
-        view.layer.cornerRadius = 20
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
         
-        view.frame = CGRect.init(x: 50, y: 50, width: 100, height: 100)
-        
-        let tapGestureRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(onClick))
-        view.addGestureRecognizer(tapGestureRecognizer)
-        view.isUserInteractionEnabled = true
+        view.backgroundColor = .darkGray
         
         return view
     }()
     
-    private lazy var btn1: UIButton = {
-        let btn = UIButton()
-        
-        btn.setTitle("Button is enabled", for: .normal)
-        btn.setTitle("Button is disabled", for: .disabled)
-        
-        btn.setTitleColor(UIColor.systemGreen, for: .normal)
-        btn.setTitleColor(UIColor.gray, for: .disabled)
-        
-        btn.frame = CGRect.init(x: 50, y: 200, width: 150, height: 40)
-        
-        btn.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
-        
-        btn.addTarget(self, action: #selector(onBtnClick), for: .touchUpInside)
-        
-        return btn
-    }()
-    
-    private lazy var image1: UIImageView = {
-        let imageView = UIImageView()
-        
-        imageView.image = UIImage.init(named: "bird")
-        imageView.contentMode = .scaleAspectFit
-        
-        imageView.frame = CGRect.init(x: 50, y: 250, width: 100, height: 100)
-        
-        return imageView
-    }()
-    
-    private lazy var slider1: UISlider = {
-        let slider = UISlider()
-        
-        slider.frame = CGRect.init(x: 50, y: 380, width: 300, height: 30)
-        
-        slider.addTarget(self, action: #selector(onSliderChange(_:)), for: .valueChanged)
-        
-        return slider
-    }()
-    
-    private lazy var stackView1: UIStackView = {
+    private lazy var stackView: UIStackView = {
         let view = UIStackView()
+        
+        view.backgroundColor = .lightGray
         
         view.axis = .vertical
-        view.distribution = .fillEqually
+        view.distribution = .equalSpacing
         view.spacing = 16
         
-        view.frame = CGRect.init(
-            x: 20,
-            y: 40,
-            width: self.view.frame.width - 40,
-            height: 1000)
-        
-        view.backgroundColor = .lightGray
-        
         return view
     }()
     
-    private lazy var viewChild1: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemBlue
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
         
-        return view
+        label.numberOfLines = 0
+        
+        label.backgroundColor = .lightGray
+        
+        label.textColor = .black
+        
+        label.font = UIFont.systemFont(ofSize: 24)
+        
+        label.textAlignment = .center
+                
+        return label
     }()
     
-    private lazy var viewChild2: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemRed
+    private lazy var textLabel2: UILabel = {
+        let label = UILabel()
         
-        return view
-    }()
-    
-    private lazy var viewChild3: UIView = {
-        let view = UIView()
-        view.backgroundColor = .yellow
+        label.numberOfLines = 0
         
-        return view
-    }()
-    
-    private lazy var viewChild4: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemBlue
-
-        return view
-    }()
-    
-    private lazy var viewChild5: UITextField = {
-        let view = UITextField()
+        label.backgroundColor = .lightGray
         
-        view.placeholder = "Write here"
+        label.textColor = .black
         
-        view.backgroundColor = .white
-        view.font = UIFont.systemFont(ofSize: 32, weight: .regular)
-        view.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18)
         
-        return view
-    }()
-    
-    private lazy var scrollView1: UIScrollView = {
-        let view = UIScrollView()
-        
-        view.frame = CGRect.init(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        
-        view.contentSize = CGSize.init(width: self.view.frame.width, height: self.stackView1.frame.height + 50)
-        
-        return view
-    }()
-    
-    private lazy var scrollView2: UIScrollView = {
-        let view = UIScrollView()
-        
-        view.frame = CGRect.init(x: 0, y: 0, width: self.view.frame.width, height: 152 + 16)
-        
-        view.contentSize = CGSize.init(width: self.stackView2.frame.width + 20, height: 152 + 16)
-        
-        view.bounces = false
-        view.contentInset = UIEdgeInsets.init(
-            top: 8,
-            left: 10,
-            bottom: 8,
-            right: 10
-        )
-        
-        return view
-    }()
-    
-    private lazy var stackView2: UIStackView = {
-        let view = UIStackView()
-        
-        view.axis = .horizontal
-        view.distribution = .fillEqually
-        view.spacing = 8
-        
-        view.frame = CGRect.init(
-            x: 0,
-            y: 0,
-            width: 800,
-            height: 152)
-        
-        view.backgroundColor = .lightGray
-        
-        return view
+        label.textAlignment = .left
+                
+        return label
     }()
     
     // MARK: - Parent delegates
@@ -177,54 +73,57 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = .white
         
-//        self.view.addSubview(self.view1)
-//        self.view.addSubview(self.btn1)
-//        self.view.addSubview(self.image1)
-//        self.view.addSubview(self.slider1)
-        
-        self.view.addSubview(self.scrollView1)
-        
-        self.scrollView1.addSubview(self.stackView1)
+//        self.view.addSubview(self.redView)
+//
+//        self.redView.snp.makeConstraints { make in
+//            make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).offset(20)
+//            make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).offset(-20)
+//            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(20)
+//            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-20)
+//        }
+//
+//        self.redView.addSubview(self.textLabel)
+//
+//        self.textLabel.snp.makeConstraints { make in
+//            make.bottom.equalTo(self.redView.snp.centerY)
+//            make.right.equalTo(self.redView.snp.centerX).offset(-8)
+//            make.left.equalToSuperview().offset(16)
+//        }
+//
+        let shortText = "Swift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager\nSwift Package Manager Swift Package Manager"
 
-        self.stackView1.addArrangedSubview(self.viewChild1)
-        self.stackView1.addArrangedSubview(self.viewChild2)
-        self.stackView1.addArrangedSubview(self.viewChild3)
-        self.stackView1.addArrangedSubview(self.viewChild4)
-        self.stackView1.addArrangedSubview(self.viewChild5)
+        self.textLabel.text = shortText
+//
+//        self.redView.addSubview(self.textLabel2)
+//
+//        self.textLabel2.snp.makeConstraints { make in
+//            make.bottom.equalTo(self.redView.snp.centerY)
+//            make.left.equalTo(self.redView.snp.centerX).offset(8)
+//            make.right.equalToSuperview().offset(-16)
+//        }
+//
+        self.textLabel2.text = "Swift Package Manager Description Description\nSwift Package Manager Description Description\nSwift Package Manager Description Description\nSwift Package Manager Description Description\nSwift Package Manager Description Description\nSwift Package Manager Description Description\nSwift Package Manager Description Description\n"
         
-        self.stackView1.addArrangedSubview(self.scrollView2)
-        self.scrollView2.addSubview(self.stackView2)
         
-        for _ in 0...8 {
-            let childView = self.getRectangleView(152)
-            self.stackView2.addArrangedSubview(childView)
+        self.view.addSubview(self.scrollView)
+        
+        self.scrollView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left)
+            make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
         }
-    }
-    
-    func getRectangleView(_ size: Int) -> UIView {
-        let view = UIView()
         
-        view.backgroundColor = .systemBlue
+        self.scrollView.addSubview(self.stackView)
         
-        view.frame = CGRect.init(x: 0, y: 0, width: size, height: size)
+        self.stackView.snp.makeConstraints { make in
+            make.top.equalTo(self.scrollView.contentLayoutGuide.snp.top).offset(16)
+            make.bottom.equalTo(self.scrollView.contentLayoutGuide.snp.bottom).offset(-16)
+            make.left.equalTo(self.scrollView.contentLayoutGuide.snp.left).offset(16)
+            make.centerX.equalTo(self.scrollView.snp.centerX)
+        }
         
-        return view
-    }
-    
-    // MARK: - Click handlers
-    
-    @objc func onClick() {
-        print("on clicked")
-        self.btn1.isEnabled.toggle()
-    }
-    
-    @objc func onBtnClick() {
-        print("on btn clicked")
-    }
-    
-    @objc func onSliderChange(_ slider: UISlider) {
-        print("on slider changed: \(slider.value)")
-        
-        self.view1.backgroundColor = UIColor.init(red: CGFloat(slider.value), green: 0.5, blue: 0.5, alpha: 1.0)
+        self.stackView.addArrangedSubview(self.textLabel)
+        self.stackView.addArrangedSubview(self.textLabel2)
     }
 }
