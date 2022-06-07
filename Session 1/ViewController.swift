6351bf33fa0535003d8781ad04f4f698d27585b3
Karim Karimov <karimkarimov@172-10-255-228.lightspeed.fyvlar.sbcglobal.net>
2022-06-07 21:09:49 +0400

    session 16

--- Session 1/ViewController.swift
+++ Session 1/ViewController.swift
@@ -8,127 +8,308 @@
 import UIKit
 import SnapKit
 import Alamofire
+import Promises
+import RxSwift
+import RxRelay
 
 class ViewController: UIViewController {
     
-    private var accessToken = "token1"
-    private var refreshToken = "11111"
+    let crtBase64 = "MIIFFzCCA/+gAwIBAgISBA1MJx6IatcwFed2q9Fqf/ZiMA0GCSqGSIb3DQEBCwUAMDIxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MQswCQYDVQQDEwJSMzAeFw0yMjA1MjIxOTUxMDhaFw0yMjA4MjAxOTUxMDdaMBQxEjAQBgNVBAMTCXN3YXBpLmRldjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANrl/w0yNiqhFI1qLGAnnhBVxsQ3w3/KGjVGZsBAG/OrerXVg0kcZgqUkq8YNlPhzRTD47Rd2nfR7AjVwNKjQQFSIJSOHnj1SWEBeuI/H5lnL0mIKVbLex95JKHQrAxwj6MFB2jsFalvLoDtwdWLYf7R/OSbY8vqNu1cCw1Hif8I51QAmWavVVs/1wZWc/BRrphHgXUkAX/YzXx1g5G5x7J2xbz6kLf+fXJoH3rf/+PIlZL7JnirHnpqaAYFBrMBUXvGXruuWo5+xUAvjbnYXrN/803G60/uYUDR1rjkm0XOyqQvyO8GJBbys4Y7UyIHUzentUuVzY1oWq5XYF8WSZECAwEAAaOCAkMwggI/MA4GA1UdDwEB/wQEAwIFoDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwDAYDVR0TAQH/BAIwADAdBgNVHQ4EFgQU02s3miYklAUNPlM3x/dFByYutiswHwYDVR0jBBgwFoAUFC6zF7dYVsuuUAlA5h+vnYsUwsYwVQYIKwYBBQUHAQEESTBHMCEGCCsGAQUFBzABhhVodHRwOi8vcjMuby5sZW5jci5vcmcwIgYIKwYBBQUHMAKGFmh0dHA6Ly9yMy5pLmxlbmNyLm9yZy8wFAYDVR0RBA0wC4IJc3dhcGkuZGV2MEwGA1UdIARFMEMwCAYGZ4EMAQIBMDcGCysGAQQBgt8TAQEBMCgwJgYIKwYBBQUHAgEWGmh0dHA6Ly9jcHMubGV0c2VuY3J5cHQub3JnMIIBAwYKKwYBBAHWeQIEAgSB9ASB8QDvAHYA36Veq2iCTx9sre64X04+WurNohKkal6OOxLAIERcKnMAAAGA7Ym79QAABAMARzBFAiBBucwT8b7fSsv18QnBASkJAdt4gafUdl/j2ta58cTKRQIhAJxhuN0zcYXPWTapI/ZlSPCHnImxoF4fAOouaounScfmAHUARqVV63X6kSAwtaKJafTzfREsQXS+/Um4havy/HD+bUcAAAGA7Ym8FwAABAMARjBEAiBresVy0ovmK75neP7OaU9yTBiVvPn1uHXexpacwZLzCQIgEs0SFVAmGVWpmIWHwhBBbNC4V9DSBRUhicIxNk0J4CUwDQYJKoZIhvcNAQELBQADggEBAKTUyJsJgiG7/8xLFg72m5N0gx4wZq8j/blf2n46D0pqBng/BRcNpiDvbf1khCyQ0+KX6YY0kLGyl39S0rtMm0opFjSykUvYHxbz4Gba4yqfizBb4HAgxR0DmvIKovVfaRRwNV+cSPcQk42Sm7dgPBrOZYDbYmP2yJZU5QzM614lYGDWrmisaQKKns/URl+hnWLpvR26oym4FKerJS3eDI1Wj/XXvybDCrXCFiLHJ/MMlUmz/vjxfwtvK6rNWq6vuDMqfHnhoJqJKOwWmCeuXpCXAG6V2p44vUdvntNbUBJv8GTTWevXZ8CoM2GCggcEEe27ixOQgmGTubAd4GO2FnY="
     
     lazy var session: Session = {
-        let authAdapter = AuthAdapter.init {
-            return self.accessToken
-        }
+        var evaluators: [String: ServerTrustEvaluating] = [:]
         
-        let authRetrier = AuthRetrier.init {
-            return self.refreshToken
-        } onUpdateAccessToken: { tokenData in
-            self.accessToken = tokenData.accessToken
-            self.refreshToken = tokenData.refreshToken
+        
+        if let certificateData = Data(base64Encoded: crtBase64, options: []),
+            let certificate = SecCertificateCreateWithData(nil, certificateData as CFData) {
+            //  use certificate to initialize PinnedCertificatesTrustEvaluator, or ...
+            
+            let serverTrustEvaluating: ServerTrustEvaluating = .pinnedCertificates(
+                certificates: [certificate],
+                acceptSelfSignedCertificates: false,
+                performDefaultValidation: true,
+                validateHost: true)
+            
+            evaluators["swapi.dev"] = serverTrustEvaluating
         }
-
         
         let interceptor = Interceptor(
-            adapters: [authAdapter],
-            retriers: [authRetrier],
+            adapters: [],
+            retriers: [],
             interceptors: [])
         
-        let session = Session(interceptor: interceptor)
+        let session = Session(
+            interceptor: interceptor,
+
+            serverTrustManager: ServerTrustManager.init(
+                evaluators: evaluators
+            )
+        )
         return session
     }()
     
+    var disposeBag: DisposeBag? = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
-        fetchUsers()
-    }
-    
-    func fetchUsers() {
-//        let request = AF.request("http://0.0.0.0:3001/users", headers: ["Authorization": "token1"])
-//
-//        request.responseJSON { data in
-//            print(data)
-//        }
-        
-        let request = self.session.request("http://0.0.0.0:3001/users")
-        
-        request
-            .validate()
-            .responseJSON { data in
-            print(data)
-        }
-        
-//        request.responseDecodable(of: [User].self) { response in
-//            print(response.value)
-//        }
-    }
-}
+        fetchUser()
+            .then { user -> Promise<[Computer]> in
+                print(user)
+                return self.fetchComputers()
+            }
+            .then { computers -> Promise<String> in
+                print(computers)
 
-class AuthAdapter: Alamofire.RequestAdapter {
-    
-    var getToken: () -> String
-    
-    init(getToken: @escaping () -> String) {
-        self.getToken = getToken
-    }
-
-    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-        var urlRequest = urlRequest
-        
-        urlRequest.setValue(getToken(), forHTTPHeaderField: "Authorization")
-        
-        completion(.success(urlRequest))
-    }
-}
-
-class AuthRetrier: Alamofire.RequestRetrier {
-    
-    var getRefreshToken: () -> String
-    var onUpdateAccessToken: (AuthToken) -> Void
-    
-    init(getRefreshToken: @escaping () -> String,
-         onUpdateAccessToken: @escaping (AuthToken) -> Void) {
-        self.getRefreshToken = getRefreshToken
-        self.onUpdateAccessToken = onUpdateAccessToken
-    }
-    
-    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
-        
-        print("error catched")
-        
-        if request.response?.statusCode == 401 {
-            print("cathed 401")
-            
-            let request = AF.request(
-                "http://0.0.0.0:3001/refresh-token",
-                headers: ["Refresh-token": getRefreshToken()])
-            request.responseDecodable(of: AuthToken.self) { response in
-                if let data = response.value {
-                    self.onUpdateAccessToken(data)
-                    completion(.retry)
-                } else {
-                    completion(.doNotRetry)
+                return Promise<String> {
+                    return "Success"
                 }
             }
-        } else {
-            completion(.doNotRetry)
+            .then{ message in
+                print(message)
+            }
+            .catch { error in
+                print(error)
+            }
+        
+//        all(fetchUser(), fetchComputers())
+//            .always {
+//                print("completed all")
+//            }
+//            .then({ user, computer in
+//                print("print all")
+//                print(user)
+//                print(computer)
+//            })
+//            .catch({ error in
+//                print(error)
+//            })
+                    
+//        any(fetchUser(), fetchComputers())
+//            .always {
+//                print("completed any")
+//            }
+//            .then({ user, computers in
+//                print("print any")
+//
+//                switch user {
+//                case .value(let safeUser):
+//                    print("safe user \(safeUser)")
+//                case .error(let userError):
+//                    print("error user \(userError)")
+//                }
+//
+//                if let safeUser = user.value {
+//                    print("safe user \(safeUser)")
+//                }
+//
+//                if let safeComputers = computers.value {
+//                    print("safe computers \(safeComputers)")
+//                }
+//            })
+//            .catch({ error in
+//                print("catch any")
+//                print(error)
+//            })
+        
+//        fetchComputers().recover { error -> Promise<[Computer]> in
+//            print("Fetch computers error \(error)")
+//            return Promise<[Computer]>{ return [Computer.init(name: "Unknown")] }
+//        }
+//        .then { computers -> Promise<User> in
+//            print(computers)
+//            return self.fetchUser()
+//        }
+//        .then { user in
+//            print(user)
+//        }.catch { error in
+//            print("general error \(error)")
+//        }
+    
+//        fetchUserCallbackPromise()
+//            .then { user in
+//                print(user)
+//            }
+//            .catch { error in
+//                print(error)
+//            }
+        
+        
+//        let fibonacciSequence = Observable.from([0,1,1,2,3,5,8])
+//
+//        let subscription = fibonacciSequence.subscribe { value in
+//            print(value)
+//        } onError: { error in
+//            print(error)
+//        } onCompleted: {
+//            print("on completed")
+//        } onDisposed: {
+//            print("on disposed")
+//        }
+//
+//        subscription.disposed(by: disposeBag!)
+        
+//        let asyncSubj = AsyncSubject<Int>.init()
+//
+//        let subscriptionAsync = asyncSubj.subscribe { received in
+//            guard let value = received.element else { return }
+//
+//            print(value)
+//        }
+//
+//        subscriptionAsync.disposed(by: disposeBag!)
+//
+//        asyncSubj.onNext(1)
+//        asyncSubj.onNext(2)
+//        asyncSubj.onNext(3)
+//        asyncSubj.onCompleted()
+        
+        
+        let behaviorSubj = ReplayRelay<Int>.create(bufferSize: 5)
+        
+        let subscriptionBehav = behaviorSubj.subscribe { received in
+            guard let value = received.element else { return }
+            
+            print("subscribe 1: \(value)")
         }
+        
+        subscriptionBehav.disposed(by: disposeBag!)
+        
+        
+        behaviorSubj.accept(1)
+        behaviorSubj.accept(2)
+        behaviorSubj.accept(3)
+        
+        let subscriptionBehav2 = behaviorSubj.subscribe { received in
+            guard let value = received.element else { return }
+            
+            print("subscribe 2: \(value)")
+        }
+        
+        subscriptionBehav2.disposed(by: disposeBag!)
+        
+        behaviorSubj.accept(4)
+        behaviorSubj.accept(5)
+        
+        let subscriptionBehav3 = behaviorSubj.subscribe { received in
+            guard let value = received.element else { return }
+            
+            print("subscribe 3: \(value)")
+        }
+        
+        subscriptionBehav3.disposed(by: disposeBag!)
+        
+        behaviorSubj.accept(6)
     }
     
-}
-
-struct AuthToken: Decodable {
-    let accessToken: String
-    let refreshToken: String
+        func fetchUser() -> Promise<User> {
+            let promise = Promise<User>.pending()
     
-    enum CodingKeys: String, CodingKey {
-        case accessToken = "access_token",
-             refreshToken = "refresh_token"
+            let request = self.session.request("https://swapi.dev/api/starships")
+    
+            request
+                .validate()
+                .responseDecodable(of: User.self) { response in
+                    if let err = response.error {
+                        promise.reject(err)
+                        return
+                    }
+    
+                    guard let user = response.value else {
+                        promise.reject(DataError(message: "Parsing error"))
+                        return
+                    }
+    
+                    promise.fulfill(user)
+                }
+    
+            return promise
+        }
+    
+//    func fetchUser() -> Promise<User> {
+//        let promise = Promise<User>(on: .main) { fulfill, reject in
+//            let request = self.session.request("http://0.0.0.0:3000/user")
+//
+//            request
+//                .validate()
+//                .responseDecodable(of: User.self) { response in
+//                    if let err = response.error {
+//                        reject(err)
+//                        return
+//                    }
+//
+//                    guard let user = response.value else {
+//                        reject(DataError(message: "Parsing error"))
+//                        return
+//                    }
+//
+//                    fulfill(user)
+//                }
+//        }
+//
+//        return promise
+//    }
+//
+//    func fetchUserCallback(onResult: @escaping (User) -> Void){
+//        let request = self.session.request("http://0.0.0.0:3000/user")
+//
+//        request
+//            .validate()
+//            .responseDecodable(of: User.self) { response in
+//                if let _ = response.error {
+//                    return
+//                }
+//
+//                guard let user = response.value else {
+//                    return
+//                }
+//
+//                onResult(user)
+//            }
+//    }
+    
+//    func fetchUserCallbackPromise() -> Promise<User> {
+//        return wrap { handler in
+//            self.fetchUserCallback(onResult: handler)
+//        }
+//    }
+//
+    func fetchComputers() -> Promise<[Computer]> {
+        let promise = Promise<[Computer]>.pending()
+
+        let request = self.session.request("http://0.0.0.0:3000/computers")
+
+        request
+            .validate()
+            .responseDecodable(of: [Computer].self) { response in
+                if let err = response.error {
+                    promise.reject(err)
+                    return
+                }
+
+                guard let computers = response.value else {
+                    promise.reject(DataError(message: "Parsing error"))
+                    return
+                }
+
+                promise.fulfill(computers)
+            }
+
+        return promise
     }
 }
 
 struct User: Decodable {
-    let id: String
+    let email: String
+    let name: String
+}
+
+struct Computer: Decodable {
+    let name: String
+}
+
+struct DataError: Error {
+    let message: String
 }
